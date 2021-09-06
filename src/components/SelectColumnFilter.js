f6f2867e4918863c9e4e5caf5f9be1c08f75dec2
Marco <marco.espinoza@grow-analytics.com.pe>
2021-09-06 17:07:05 -0500

    Creacion de tabla con paginacion, filtros de columna y ocultar columnas

---
+++ src/components/SelectColumnFilter.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+
+export const SelectColumnFilter = ({column}) => {
+
+    const [state, setstate] = useState([])
+
+    const { filterValue, setFilter, preFilteredRows, id } = column
+    // Calculate the options for filtering
+    // using the preFilteredRows
+    const options = React.useMemo(() => {
+      const options = new Set()
+      preFilteredRows.forEach(row => {
+        options.add(row.values[id])
+      })
+      return [...options.values()]
+    }, [id, preFilteredRows])
+  
+    // Render a multi-select box
+    return (
+      <select
+        value={filterValue}
+        onChange={e => {
+            console.log(state)
+            state.push(e.target.value)
+            // setstate(nuevoArray)  
+            console.log(state)
+            setFilter(state)
+
+        }}
+      >
+        <option value="">All</option>
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    )
+  }
